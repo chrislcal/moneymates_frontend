eafe5f28b09cd4135beef03aaff705b8f55bbdc0
Julia Kolesnikova <julikole@gmail.com>
2023-02-06 13:48:31 +0100

    Added Goals and RecurrentPayments to components, updated package-lock file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
   return (
     <div className="App">
-      hei
+      <h1>MoneyMates</h1>
     </div>
   );
 }
