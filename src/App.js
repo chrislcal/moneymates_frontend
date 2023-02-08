085702340ed0ec6b9ab5090772b1e12258fa3671
Julia Kolesnikova <julikole@gmail.com>
2023-02-08 14:18:29 +0100

    Added CategoryCard component to the App-file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
 import './App.css';
+import './styles/nav.css';
+import './styles/categoryCard.css';
+
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/Nav.js'
+import Navbar from './components/Nav.js';
+import CategoryCard from './components/CategoryCard.js';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1 className="app-name">MoneyMates</h1>
-      <Navbar/>
+      <header>
+        <h1 className="app-name">MoneyMates</h1>
+        <Navbar/>
+      </header>
+      <CategoryCard />
     </div>
   );
 }
