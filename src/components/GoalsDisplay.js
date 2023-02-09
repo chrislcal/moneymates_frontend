1547118684f345d04dc79c59338dbedf533277fb
Julia Kolesnikova <julikole@gmail.com>
2023-02-09 15:33:27 +0100

    added GoalsDisplay-component

---
+++ src/components/GoalsDisplay.js
@@ -0,0 +1,19 @@
+import { useLocation } from "react-router-dom";
+
+
+
+const GoalsDisplay = () => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const name = params.get("name");
+  const description = params.get("description");
+
+  return (
+    <div>
+      <h1 style={{ margin: "auto" }}>{name}</h1>
+      <p style={{ margin: "auto" }}>{description}</p>
+    </div>
+  );
+};
+
+export default GoalsDisplay;
