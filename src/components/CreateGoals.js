349c53c00cea37c337a2387cbe58e82b979631a9
BewarPiri <122524395+BewarPiri@users.noreply.github.com>
2023-02-10 13:46:05 +0100

    removed link

--- src/components/CreateGoals.js
+++ src/components/CreateGoals.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Link } from "react-router-dom";
+
 
 const CreateGoals = () => {
   const user1_accounts = {
