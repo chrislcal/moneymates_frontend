08c9bac4c73526796fa675cbb5a0ff7bd74b00a2
chrislcal <chrislcal96@gmail.com>
2023-02-14 11:05:18 +0100

    Updating

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,3 +1,5 @@
+
+
 import ProgressBar from './ProgressBar';
 import { useHistory } from 'react-router-dom';
 import {useEffect, useState} from 'react';
@@ -9,8 +11,28 @@ function Goals() {
     const {user, getAccessTokenSilently} = useAuth0();
 
     const [goals, setGoals] = useState([]);
+    const [bankData, setBankData] = useState([]);
 
     useEffect(() => {
+        const getBalances = async() => {
+            try {
+                const request = await fetch('http://localhost:3001/universal', {
+                    method: 'GET',
+                    headers: {
+                        token: await getAccessTokenSilently()
+                    }
+                });
+
+                const data = await request.json();
+                
+                setBankData(data);
+                console.log(data);
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
         const getGoals = async() => {
             try {
                 const request = await fetch('http://localhost:3001/get-goals', {
@@ -28,33 +50,44 @@ function Goals() {
             }
         }
          getGoals()
+         getBalances()
     }, []);
 
     console.log(goals);
 
-   
-    const testData = { bgcolor: "#5D1788", completed: 90 };
     let history = useHistory();
 
-   
-    
+    let allGoals;
+
+    if(goals.length > 0 && bankData.length > 0) {
+        allGoals = goals.map((goal) => {
+            const selectedAccount = bankData.find(account => account.details.product === goal[3]);
+            const balance = selectedAccount ? selectedAccount.balances.balanceAmount.amount : 0;
+            const percentage = (balance / goal[2]) * 100;
+
+            return(
+                <div className="goal-card" key={goal[0]}>
+                <div className="progress-bar">
+                    <ProgressBar bgcolor="#BDBDBD" completed={percentage.toFixed(2)} />
+                </div>
+                <h1 className="goal-name">{goal[0]}</h1>
+
+                <div className="goal-description">
+                    <p>{goal[1]}</p>
+                </div>
+            </div>
+            )
+        });
+    }
+
     const handleAddGoal = () => {
         history.push('/create-goals');
     }
 
     return (
         <div className="goals-container">
+            {allGoals}
 
-            <div className="goal-card">
-                <div className="progress-bar">
-                    <ProgressBar bgcolor={testData.bgcolor} completed={testData.completed} />
-                </div>
-                <h1 className="goal-name">Aiya Napa</h1>
-
-                <div className="goal-description">
-                    <p>Go Ayia Napa daddyboys!</p>
-                </div>
-            </div>
             <button className="add-goal-button" onClick={handleAddGoal}>
                 Add new goal
             </button>
